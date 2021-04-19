941ed3d3e72847a3ea2391c8c2c6d8b4d0164fb8
TonyJoaca <dj_tony2013@yahoo.ro>
2021-04-19 09:29:36 +0000

    dipper:Octavi OS bringup

--- device.mk
+++ device.mk
@@ -110,6 +110,20 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     SecureElement
 
+#Octavi HALS
+YOUR_HW_PLATFORM := sdm845
+
+# Hardware
+PRODUCT_BOARD_PLATFORM := $(YOUR_HW_PLATFORM)
+PRODUCT_USES_QCOM_HARDWARE := true
+
+# HALS
+SRC_AUDIO_HAL_DIR := hardware/qcom-caf/$(YOUR_HW_PLATFORM)/audio
+SRC_DISPLAY_HAL_DIR := hardware/qcom-caf/$(YOUR_HW_PLATFORM)/display
+SRC_MEDIA_HAL_DIR := hardware/qcom-caf/$(YOUR_HW_PLATFORM)/media
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH) \
+    hardware/qcom-caf/$(YOUR_HW_PLATFORM) \
     vendor/nxp/opensource/pn5xx
